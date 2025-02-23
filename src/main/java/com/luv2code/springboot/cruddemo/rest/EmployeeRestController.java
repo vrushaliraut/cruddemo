7f2fc1bd218e7e9ef19ebd7d64b4967c7d95a3db
vrushali_raut <vraut.c@roshn.sa>
2025-02-23 18:42:49 +0530

    crud demo in springboot with service layer and jpa

---
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -0,0 +1,38 @@
+package com.luv2code.springboot.cruddemo.rest;
+
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    // private final EmployeeDAO employeeDAO;
+    // Quick and dirty: inject employee dao (use constructor injection)- refactor later
+
+//    public EmployeeRestController(EmployeeDAO employeeDAO) {
+//        this.employeeDAO = employeeDAO;
+//    }
+
+    private EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeRestController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    // Expose "/employees" and return a list of employees
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        // employeeDAO.findAll();
+        return employeeService.findAll();
+    }
+
+}
